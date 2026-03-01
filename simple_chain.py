343333921053986952bc16b131fb0ccc3a455ff4
ayushkotadiya5499 <142094873+ayushkotadiya5499@users.noreply.github.com>
2026-03-01 09:40:38 +0530

    add prompttemplate

--- simple_chain.py
+++ simple_chain.py
@@ -1,9 +1,21 @@
 from langchain_openai.chat_models import ChatOpenAI
+from langchain_core.prompts import PromptTemplate
 from dotenv import load_dotenv
 import os
 
 load_dotenv()
 
+
+template=PromptTemplate(template="""
+
+        give me answer of this question  : {question} in less than 50 words
+                        """,
+                        input_variables=['question'],
+                        validate_template=True)
+
+
 model=ChatOpenAI(api_key=os.getenv("OPENAI_API_KEY"), model="gpt-3.5-turbo", temperature=0.9)
-response=model.invoke("What is the capital of India? and famous food").content
+
+x=template.invoke('what is capital of india')
+response=model.invoke(x).content
 print(response)
