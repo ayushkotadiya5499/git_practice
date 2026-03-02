138574093d9a2db2dfda3867425c44325a7a040e
ayush <ayushk@webelight.co.in>
2026-03-02 13:28:36 +0530

    add ml testing

---
+++ test/test.py
@@ -0,0 +1,11 @@
+import  pandas as pd
+from sklearn.linear_model import LinearRegression
+from sklearn.metrics import mean_squared_error
+from sklearn.datasets import make_regression
+
+X, y = make_regression(n_samples=100, n_features=1, noise=10, random_state=42)
+model = LinearRegression()
+model.fit(X, y)
+y_pred = model.predict(X)
+mse = mean_squared_error(y, y_pred)
+print(f"Mean Squared Error: {mse}")
